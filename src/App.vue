403c3ed6f42fd35f90b4c3387c19d9607b230283
Tuong Huynh <tuong.huynh@hdwebsoft.co>
2020-05-20 18:07:51 +0700

    create vue test video

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="video-wrap">
+      <video class="video" controls loop>
+        <source src="./assets/8k.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+.video {
+  width: 100%;
+  height: 100%;
+}
+.video-wrap {
+  width: 100%;
+  height: 100%;
+}
+</style>
